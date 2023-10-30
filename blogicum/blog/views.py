2469056b8a3cbe98d9d6318b9a65f3051915229b
Spirual <spiuaal@gmail.com>
2023-10-30 23:00:53 +0300

    Отформатированы импорты.

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
 from django.db.models import Count
-from django.http import HttpResponseForbidden, Http404
+from django.http import Http404
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy, reverse
 from django.utils import timezone
